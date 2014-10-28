e60b8a7801e4ca8d568849d9a93afaeb91772c18
Jougla.benoit <Jougla.benoit@gmail.com>
2014-10-28 13:24:06 +0000

    Changement de l'arborescence du projet

---
+++ chatApp.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+angular.module
+(
+	'chatApp', 
+	[
+		'ngRoute',
+		'chatApp.connectForm',
+		'chatApp.home'
+	]
+)
+
+.config
+(
+	function($routeProvider)
+	{
+		$routeProvider
+		.when
+		(
+			'/',
+			{
+				redirectTo: '/connectForm'
+			}
+		)
+		.otherwise
+		(
+			{
+				redirectTo: '/connectForm'
+			}
+		);
+	}	
+)
+
+;
