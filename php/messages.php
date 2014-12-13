548e0db4bbf21c24167637d8ed567173f2fcfd49
prouheze-maxime <prouheze-maxime@hotmail.fr>
2014-12-13 00:49:19 +0000

    ajout de l'enregistrement des messages

--- php/messages.php
+++ php/messages.php
@@ -4,9 +4,23 @@
 
 	session_start();
 	
+	$data	= array();	// array to pass back data
+	
 	if( isset($_SESSION['nickname']) )
 	{
-		echo json_encode(getMessages($_SESSION['nickname']));
+		//echo json_encode(getMessages($_SESSION['nickname']));
 	}
+	
+	if( ! isset($_POST['message']) || empty($_POST['message']) )
+	{
+		$data['success'] = false;
+	}
+	else $data['message'] = $_POST['message'];
+	
+	$data['success'] = true;
+	
+	addMessage($_SESSION['nickname'], null, $data['message']);
+	
+	echo json_encode($data);
 
 ?>
